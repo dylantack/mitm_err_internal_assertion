8a9788ca07ca04d3fdcd08c2f94f7d3836397642
Dylan Tack <dylan.tack@ubnt.com>
2019-07-13 21:36:21 -0700

    run lots of tests

--- __tests__/integrations.spec.ts
+++ __tests__/integrations.spec.ts
@@ -10,7 +10,7 @@ afterAll(() => {
     mock.disable();
 });
 
-test('post usage 1', async () => {
+const doTest = async () => {
     const s = new Stripe('key');
     const usage = await s.usageRecords.create('id', {
         action: 'increment',
@@ -18,4 +18,8 @@ test('post usage 1', async () => {
         timestamp: 10,
     });
     expect(usage.id).toContain('mbur');
-});
+};
+
+for (let i=1; i < 100; i++) {
+    test(`test ${i}`, doTest);
+}
