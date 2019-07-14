9aae4c720f5daaa38dd07b6c7cf739d9d63c8e27
Dylan Tack <dylan.tack@ubnt.com>
2019-07-13 22:59:01 -0700

    reduce more

--- __tests__/integrations.spec.ts
+++ __tests__/integrations.spec.ts
@@ -1,14 +1,12 @@
 import Mitm from 'mitm';
 import Stripe from 'stripe';
 
-const mitm = Mitm();
-mitm.on('request', (req, res) => {
+Mitm().on('request', (req, res) => {
     res.end(JSON.stringify({id:'hello'.repeat(50)}));
 });
 
 const doTest = async () => {
-    const s = new Stripe('key');
-    const usage = await s.usageRecords.create('id', {
+    await new Stripe('key').usageRecords.create('id', {
         quantity: 10,
         timestamp: 10,
     });
