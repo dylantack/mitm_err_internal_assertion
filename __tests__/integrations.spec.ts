84c9c212d46020959b85007f1766612933ef92b5
Dylan Tack <dylan.tack@ubnt.com>
2019-07-13 22:28:32 -0700

    reduce more

--- __tests__/integrations.spec.ts
+++ __tests__/integrations.spec.ts
@@ -13,11 +13,10 @@ afterAll(() => {
 const doTest = async () => {
     const s = new Stripe('key');
     const usage = await s.usageRecords.create('id', {
-        action: 'increment',
         quantity: 10,
         timestamp: 10,
     });
-    expect(usage.id).toContain('mbur');
+    expect(usage.id).toContain('hello');
 };
 
 for (let i=1; i < 100; i++) {
--- __tests__/mock-apis/index.ts
+++ __tests__/mock-apis/index.ts
@@ -1,32 +1,12 @@
-import Router from 'koa-router';
-import bodyparser from 'koa-bodyparser';
-import Koa from 'koa';
 import Mitm from 'mitm';
 
 export default function() {
     const mitm = Mitm();
 
     mitm.on('request', (req, res) => {
-        stripeHandler(req, res);
+        res.statusCode = 200;
+        res.end(JSON.stringify({id:'hello'.repeat(50)}));
     });
 
     return mitm;
 }
-
-const router = new Router();
-router.post('/v1/subscription_items/:item_id/usage_records', async ctx => {
-    const { quantity, timestamp } = ctx.request.body as any;
-    ctx.body = {
-        id: 'mbur_12345' ,
-        object: 'usage_record',
-        livemode: false,
-        quantity,
-        subscription_item: 'si_12345',
-        timestamp,
-    };
-});
-
-const stripeHandler = new Koa()
-    .use(bodyparser())
-    .use(router.routes())
-    .callback();
