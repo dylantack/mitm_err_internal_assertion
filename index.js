77004404332c8e6faa5d2ba2d57d39e583db0060
Dylan Tack <dylan.tack@ubnt.com>
2019-07-14 00:24:25 -0700

    reduce - eliminate stripe

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-var Mitm = require("mitm");
-var Stripe = require("stripe");
+const Mitm = require("mitm");
+const https = require("https");
+
 
 Mitm().on('request', (req, res) => {
     res.end(JSON.stringify({id:'hello'.repeat(50)}));
 });
 
+const agent = new https.Agent({keepAlive: true});
+const request = () => {
+    return new Promise((resolve, reject) => {
+        https.get('https://example.com', {agent}, (res) => {
+            res.on('data', d => {});
+            res.on('end', resolve);
+        });
+    });
+}
+
 async function main() {
     for (let i=1; i < 100; i++) {
-        await new Stripe('key').usageRecords.create('id', {
-            quantity: 10,
-            timestamp: 10,
-        });
+        const foo = await request();
     }
 }
 
